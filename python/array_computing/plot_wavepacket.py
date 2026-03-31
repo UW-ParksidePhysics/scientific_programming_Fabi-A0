7c74128f2b3e7f65f2fdcfe31fe3338380e9d3e6
Fabi-A0 <158516899+Fabi-A0@users.noreply.github.com>
2026-03-30 23:49:39 -0500

    Update plot_wavepacket.py

--- python/array_computing/plot_wavepacket.py
+++ python/array_computing/plot_wavepacket.py
@@ -3,28 +3,31 @@ import matplotlib.pyplot as plt
 
 
 def wave_packet(x, t, alpha, f, k, omega):
-    value = np.exp(-(alpha * x - f * t)**2) * np.sin(k * x - omega * t)
-    return value
+    return np.exp(-(alpha * x - f * t)**2) * np.sin(k * x - omega * t)
 
 
 if __name__ == "__main__":
+    # given parameters
     t = 0
     f = 3
     k = 3 * np.pi
     omega = 3 * np.pi
 
+    # x values in [-4, 4]
     x_values = np.linspace(-4, 4, 1001)
 
+    # alpha values
     alpha_values = [0.5, 1, 2]
 
+    # plot each alpha
     for alpha in alpha_values:
         y_values = wave_packet(x_values, t, alpha, f, k, omega)
         plt.plot(x_values, y_values, label=f"alpha = {alpha}")
 
-    plt.title("Wave Packet")
     plt.xlabel("x")
     plt.ylabel("f(x, t)")
-    plt.grid()
+    plt.title("Wave Packet for Different Alpha Values")
     plt.legend()
+    plt.grid()
 
     plt.show()
